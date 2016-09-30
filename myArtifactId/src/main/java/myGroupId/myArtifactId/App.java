28d1ff83ad7d04a5d773248a8ea1f9966193fddc
Martin Haylezov <martin.haylezov@estafet.com>
2016-09-30 16:01:41 +0300

    New project was added

---
+++ myArtifactId/src/main/java/myGroupId/myArtifactId/App.java
@@ -0,0 +1,13 @@
+package myGroupId.myArtifactId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
